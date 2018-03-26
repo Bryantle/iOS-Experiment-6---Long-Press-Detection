4408798ea2df9dd62ecf6ae7824881e3b6da1129
Bryantle <bryantl@berkeley.edu>
2018-03-26 14:47:45 -0700

    Long Hold Gesture detection added.

--- iOS Experiment 6 - Long Press Detection/ViewController.swift
+++ iOS Experiment 6 - Long Press Detection/ViewController.swift
@@ -9,7 +9,14 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBAction func handleGesture(_ sender: UILongPressGestureRecognizer) {
+        if sender.state == UIGestureRecognizerState.ended{
+            let alertController = UIAlertController(title: nil, message: "Long-Press Gesture Detected", preferredStyle: UIAlertControllerStyle.alert)
+            alertController.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.default,handler: nil))
+            self.present(alertController, animated: true, completion: nil)
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
